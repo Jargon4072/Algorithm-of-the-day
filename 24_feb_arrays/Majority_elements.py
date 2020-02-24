9ba309b4618e3e9fa5ddc4e5a69db30a18dcd785
Chandan Dwivedi <chandan@aiseon.in>
2020-02-24 20:16:19 +0530

    array, day1 after review

--- 24_feb_arrays/Majority_elements.py
+++ 24_feb_arrays/Majority_elements.py
@@ -1,14 +1,14 @@
 
 def count_majority(arr):
     flag=0
-    hash=[0*10000000]*10000000
+    hash=[0*10000000]*10000000       #todo: optimise for memory
     for i in arr:
         val=int(i)
         hash[val]+=1             #concept of hashing is used. hash[] used to store count of each element
         if(hash[val]>(n/2)):
             flag=1
             #print(hash[val])
-            return int(i)
+            return int(i)    #todo: for multiple majority
     if (flag==0):
         return -1
 
