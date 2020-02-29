3a853d6ce82ad83f3c6229c1a11ce060f8da0370
Rama Sharma <rama.tripathi18@gmail.com>
2020-02-29 10:37:06 +0530

    formatted problem statement

--- 24_feb_arrays/leaders_of_array.py
+++ 24_feb_arrays/leaders_of_array.py
@@ -1,7 +1,8 @@
 #=========================================PROBLEM Statement ==============================================
 '''
 Given an array of positive integers. Your task is to find the leaders in the array.
-Note: An element of array is leader if it is greater than or equal to all the elements to its right side. Also, the rightmost element is always a leader.
+Note: An element of array is leader if it is greater than or equal to all the elements to its right side. 
+Also, the rightmost element is always a leader.
 
 Input:
 The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
@@ -31,7 +32,9 @@ Output:
 7 7 3
 
 Explanation:
-Testcase 3: All elements on the right of 7 (at index 0) are smaller than or equal to 7. Also, all the elements of right side of 7 (at index 3) are smaller than 7. And, the last element 3 is itself a leader since no elements are on its right.
+Testcase 3: All elements on the right of 7 (at index 0) are smaller than or equal to 7.
+ Also, all the elements of right side of 7 (at index 3) are smaller than 7. 
+And, the last element 3 is itself a leader since no elements are on its right.
 '''
 #======================================================================================================================
 
