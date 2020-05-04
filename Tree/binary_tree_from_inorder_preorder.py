31a6dd2a19c2585cb00077df28de2fe58bcb47db
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2020-05-05 01:12:16 +0530

    TODO: Optimize the search to make it O(N)

---
+++ Tree/binary_tree_from_inorder_preorder.py
@@ -0,0 +1,41 @@
+class treenode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+    def print_inorder_tree(self, root):
+        if root == None:
+            return
+        self.print_inorder_tree(root.left)
+        print(root.data,end=" ")
+        self.print_inorder_tree(root.right)
+
+def construct_tree(inorder, preorder, istart, iend):
+    if istart>iend:
+        return None
+    root=treenode(preorder[construct_tree.var])
+    construct_tree.var=construct_tree.var+1
+    if istart==iend:
+        return root
+
+    idx=search(inorder,istart,iend,root.data)
+    root.left=construct_tree(inorder,preorder,istart,idx-1)
+    root.right=construct_tree(inorder,preorder,idx+1,iend)
+    return root
+def search(arr, start, end, value):
+    for i in range(start, end + 1):
+        if arr[i] == value:
+            return i
+
+if __name__ == "__main__":
+    print("Enter Inorder Traversal sequence: ")
+    inorder = [int(x) for x in input().strip().split()]
+    print("Enter Preorder Traversal Sequence: ")
+    preorder = [int(x) for x in input().strip().split()]
+    construct_tree.var=0
+
+    root=construct_tree(inorder, preorder,0,len(inorder)-1)
+    root.print_inorder_tree(root)
+    print("")
