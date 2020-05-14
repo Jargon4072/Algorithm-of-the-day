07da92efcc1a727ac395c4084e1af287c11ba819
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2020-05-14 14:06:05 +0530

    update

--- Tree/convert_binary_tree_to_doubly_linkedlist.py
+++ Tree/convert_binary_tree_to_doubly_linkedlist.py
@@ -3,12 +3,81 @@ class treenode:
         self.data=data
         self.left=None
         self.right=None
-class doublyll:
+
+class DlNode:
     def __init__(self,data):
-        self.data=data
         self.next=None
         self.prev=None
+        self.data=data
+
+
+class doublyll:
+    def __init__(self):
+        self.head=None
+
+    def insert(self,data):
+        new_node=DlNode(data)
+        new_node.next=None
+        if self.head is None:
+            new_node.prev=None
+            self.head=new_node
+            return
+
+        tmp=self.head
+        while tmp.next is not None:
+            tmp=tmp.next
+
+        tmp.next=new_node
+        new_node.prev=tmp
+        return
+    def printll(self):
+        if self.head is not None:
+            tmp=self.head
+            while tmp is not None:
+                print(tmp.data, end=" ")
+                tmp=tmp.next
+
+            print("")
+
+def inorder(root,list):
+    if root==None:
+        return
+    inorder(root.left,list)
+    list.append(root.data)
+    inorder(root.right,list)
 
 def binary_tree2Doubly_ll(root):
     if root==None:
         return
+    val=[]
+    inorder(root,val)
+    dll=doublyll()
+    for item in val:
+        dll.insert(item)
+
+    return dll
+
+
+if __name__=="__main__":
+    root = treenode(5)
+    root.left = treenode(3)
+    root.right = treenode(6)
+    root.left.left = treenode(1)
+    root.left.right = treenode(4)
+    root.right.right = treenode(8)
+    root.left.left.left = treenode(0)
+    root.left.left.right = treenode(2)
+    root.right.right.left = treenode(7)
+    root.right.right.right = treenode(9)
+
+    dll=doublyll()
+
+    dll=binary_tree2Doubly_ll(root)
+    print("printing linkedlist elements: ")
+
+    dll.printll()
+
+
+
+
+
