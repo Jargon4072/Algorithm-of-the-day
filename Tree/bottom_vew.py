aa11f06a678b3a2c239ce6373860bca5dca06d09
Chandan Dwivedi <chhandan.dwivedi99@gmail.com>
2020-05-28 02:07:20 +0530

    tree section completed!

---
+++ Tree/bottom_vew.py
@@ -0,0 +1,22 @@
+# @Author: Dwivedi Chandan
+# @Date:   2020-05-28T02:01:11+05:30
+# @Email:  chandandwivedi795@gmail.com
+# @Last modified by:   Dwivedi Chandan
+# @Last modified time: 2020-05-28T02:07:00+05:30
+
+
+
+'''
+
+                      20
+                    /    \
+                  8       22
+                /   \      \
+              5      3      25
+                    / \
+                  10    14
+
+ output : 5, 10, 3, 14, 25.
+
+
+'''
